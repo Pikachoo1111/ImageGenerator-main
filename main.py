59594780750b4b08f1814f8a2a5969305f01fddd
Armaan <armaan@cmsnova.com>
2024-03-26 10:25:52 -0500

    Just a few updates to improve the functions

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import subprocess
 #import generateimage.py
 #import EncoderDecoder.py
 
+file_path = "/Users/armaan/Downloads/ImageGenerator-main/generateimage.py"
+
 root = tkinter.Tk()
 def setup():
     root.title("Airplane Display")
@@ -10,10 +12,17 @@ def setup():
     root.configure(bg="white")
     root.resizable(False, False)
     label = tkinter.Label(root, text="Airplane Display", font=("Arial", 24), bg="black")
-    label.place(x=960, y=50)
+    label.place(x=960, y=150)
     label.pack()
     button = tkinter.Button(root, text="Generate Image", font=("Arial", 24), bg="white")
     button.pack()
     
+
+def GenerateButton():
+    completed_process = subprocess.run(['python', file_path], capture_output=True, text=True)
+    if completed_process.returncode == 0:
+        print("Execution successful.")
+        print("Output:")
+        print(completed_process.stdout)
 setup()
 root.mainloop()
