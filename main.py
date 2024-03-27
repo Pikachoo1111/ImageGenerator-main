92cc4820512523ff0fa7e35711bfb29c52de9fc4
Armaan <armaan@cmsnova.com>
2024-03-27 12:10:25 -0500

    Fixed Bugs

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import wget
 #import generateimage.py
 #import EncoderDecoder.py
 import ssl
+import os
+import time
 
 # Disable SSL certificate verification
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -21,9 +23,12 @@ def GenerateButton():
     result1 = subprocess.check_output(command, shell=True, text=True)
     wget.download(result1, "/Users/armaan/Downloads/ImageGenerator-main/Images")
     image = Image.open("/Users/armaan/Downloads/ImageGenerator-main/Images/ai.png")
-    tk_image = ImageTk.PhotoImage(image)
+    resized_image = image.resize((500, 500))  # Resize the image to desired dimensions
+    tk_image = ImageTk.PhotoImage(resized_image)
     label = tk.Label(root, image=tk_image)
+    label.image = tk_image  # Keep a reference to the image to prevent garbage collection
     label.pack()
+    os.remove("/Users/armaan/Downloads/ImageGenerator-main/Images/ai.png")  # Remove the image after displaying it
 
 
 
